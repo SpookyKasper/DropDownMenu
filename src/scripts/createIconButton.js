e6a14d155bc04b09423e31080f41b502ab78dc2e
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-07-03 22:42:57 +0200

    Implement a dropDown button module and a icon button module

--- src/scripts/createIconButton.js
+++ src/scripts/createIconButton.js
@@ -1,9 +1,8 @@
-
 export default function createIconButton(iconFile) {
   const myButton = document.createElement('button')
-  myButton.classList.add('icon-button')
   const myImg = document.createElement('img')
   myImg.src = iconFile
+  myButton.classList.add('icon-button')
   myButton.append(myImg)
 
   return myButton
--- src/scripts/dropDown.js
+++ src/scripts/dropDown.js
@@ -1,8 +1,5 @@
-import menuIcon from '../images/menu.svg'
-import createIconButton from './createIconButton'
-
-export default function dropDown(menuEl) {
-  const dropDownBtn = createIconButton(menuIcon)
-  dropDownBtn.addEventListener('click', () => menuEl.classList.toggle('visible'))
-  return dropDownBtn
+export default function dropDownButton(buttonEl, menuEl) {
+  const myBtn = buttonEl
+  myBtn.addEventListener('click', () => menuEl.classList.toggle('visible'))
+  return myBtn
 }
